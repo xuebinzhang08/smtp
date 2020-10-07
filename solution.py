2c69610ba025f3c045dddde89289983e96a948af
zhangxuebin <xz2667@nyu.edu>
2020-10-07 18:20:03 -0400

    9.0

--- solution.py
+++ solution.py
@@ -1,6 +1,8 @@
 from socket import *
 
 def smtp_client(port=1025, mailserver='127.0.0.7'):
+    msg = "\r\n My message"
+    endmsg = "\r\n.\r\n"
     # Choose a mail server (e.g. Google mail server) if you want to verify the script beyond GradeScope
 
     # Create socket called clientSocket and establish a TCP connection with mailserver and port
@@ -25,42 +27,35 @@ def smtp_client(port=1025, mailserver='127.0.0.7'):
 
     # Send MAIL FROM command and print server response.
     # Fill in start
-    mailFrom = "MAIL FROM:<foobar>\r\n"
+    mailFrom = 'MAIL FROM:<1234@qqq.com>\r\n'
     clientSocket.send(mailFrom.encode())
     recv2 = clientSocket.recv(1024).decode()
-    #print(recv2)
     # Fill in end
 
     # Send RCPT TO command and print server response.
     # Fill in start
-    rcptTo = "RCPT TO:<lorenzo@blarghblahblargh.com>\r\n"
-    clientSocket.send(rcptTo.encode())
+    reptTo = 'RCPT TO:<1234@qqq.com>\r\n'
+    clientSocket.send(reptTo.encode())
     recv3 = clientSocket.recv(1024).decode()
-    #print(recv3)
     # Fill in end
 
     # Send DATA command and print server response.
     # Fill in start
-    dataHeader = "DATA\r\n"
-    clientSocket.send(dataHeader.encode())
+    data = 'DATA\r\n'
+    clientSocket.send(data.encode())
     recv4 = clientSocket.recv(1024).decode()
-    #print(recv4)
     # Fill in end
 
     # Send message data.
     # Fill in start
-    subject = "Subject: Hi this is a test e-mail subject from my client\r\n\r\n"
-    clientSocket.send(subject.encode())
-    msg = "\r\n I sure hope this works"
     clientSocket.send(msg.encode())
     # Fill in end
 
     # Message ends with a single period.
     # Fill in start
-    end = "\r\n.\r\n"
-    clientSocket.send(end.encode())
-    recv5 = clientSocket.recv(1024).decode()
-    #print(recv5)
+    clientSocket.send(endmsg.encode())
+    recv6 = clientSocket.recv(1024).decode()
+    # print(recv6)
     # Fill in end
 
     # Send QUIT command and get server response.
